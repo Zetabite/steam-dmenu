02920d249cb65db33d7cfe318320bc5004539b66
Adrian <zetabite.horzion@gmail.com>
2023-09-01 22:43:47 +0200

    Initial changes of fork

--- README.md
+++ README.md
@@ -1,16 +1,68 @@
 # steam-dmenu
 
-Reads installed Steam games from one or multiple Steam libraries and offers a selection in dmenu compatible format.
+Reads installed Steam apps/games from all libraries referenced in the given `libaryfolder.vdf` file. Can be used to either launch dmenu with the apps/games as selectable options, or generates shell scripts in a folder, to be used as dmenu program location, to be listed in users default dmenu.
 
 ## Requirements
-- Steam (obviously)
-- python 3
+- Steam
+- python 3.9+
+- python-vdf for reading Valve's VDF format
 - xdg-open
-- some dmenu compatible application (dmenu, rofi, bemenu, wofi...)
+- dmenu or dmenu compatible application (dmenu, rofi, bemenu, wofi...)
+
+## Significant differences in the fork
+The library parameter only allows for one library folder to be given, reason being, if a library isn't defined in the active installations `libraryfolders.vdf`, there will be issues to launch an app or game anways.
+You can hide certain apps/games, since you probably don't want tools like Proton to show up, or if you know you know.
+Enforcing python 3.9+, because I prefer the type hinting being even further integrated, as it makes it easier to understand what a variable or functions is supposed to do or store.
+Using python-vdf again, as it allows to easier read the files, but I am open to write my own parser for that.
+Customizing the display of the apps/games in dmenu.
+You can generate scripts to integrate the launching of games more tightly, so that you don't need to execute this program and then select your game.
 
 ## Configuration
+### dmenu compatible application
 While dmenu is the default, any compatible alternative or additional parameters for dmenu can be used with the `--dmenu` (`-d`) flag.
-The Steam library is assumed to be in `~/.local/share/Steam`, however the `--library` (`-l`) flag takes any number of alternative library locations.
+#### Default
+`dmenu -i`
+This means, it will use dmenu and be case-insensetive.
+#### Example for rofi:
+`steam_dmenu.py -d "rofi -dmenu -i"`
 
-### Example with rofi and 2 library locations
-`steam_dmenu.py -d 'rofi -dmenu -i' -l '~/.local/share/Steam' '~/Games/Steam'`
+### libraryfolders.vdf
+This is the location of the `libraryfolders.vdf` file. It is assumed to be in `~/.local/share/Steam/steamapps/libraryfolders.vdf`, however the `--library` (`-l`) flag can take an alternative path.
+#### Default
+`~/.local/share/Steam/steamapps/libraryfolders.vdf`
+This is default steam library folder on many distros. Usually it is outside of `steamapps`, if the library folder is not the main library. It will try to locate it in a folder or it's existing `steamapps` sub folder, if the file itself is not given.
+#### Example
+`steam_dmenu.py -l "/home/user/other/steam/library/folder/libraryfolders.vdf"`
+
+### Which prefix to show in dmenu
+If you want your apps/games to appear in dmenu with a prefix like `play`, you can use the parameter `--prefix` (`-p`).
+#### Default
+`play `
+The space is inserted, because internally it isn't further formatted and is used as is, to make the code more readable and less complex. So if you want a space inbetween the prefix and your app/game title, you have to add a space.
+#### Example "Meow " resulting in "Meow [Name of the App/Game]"
+`steam_dmenu.py -p "Meow "`
+
+### Hiding certain games from dmenu
+You can also hide certain apps/games, by giving a list of appids for the `--blocked` (`-b`) parameter. By default Proton and some other steam tools are hidden.
+#### Default
+If parameter isn't used, the output of the `default_blocked_appids` function in `steam_dmenu.py` is used. Give empty string to use no block list.
+#### Example for blocking just Team Fortress 2 and Half-Life 2
+`steam_dmenu.py -b "440,220"`
+
+### Generate scripts
+If you want to generate scripts to be used in dmenu for launching, use `--output` (`-o`). The folder needs to exist, otherwise the application will close without generating the scripts. It overwrites existing scripts that use the same prefix. It only relies on the appid, not the path of the stored application.
+#### Default
+By default doesn't generate and launches dmenu.
+#### Example with xmonad
+`steam_dmenu.py -o "~/.xmonad/dmenu/steam"`
+
+### Mode
+You can launch the custom dmenu and also generate scripts both at once. Use the parameter `--mode` (`-m`) with the corresponding mode number.
+#### Options
+`0`: Just launch
+`1`: Generate scripts
+`2`: Generate scripts, then launch
+#### Default
+`0`
+#### Example generating and launching
+`steam_dmenu.py -m 2`
--- steam_dmenu.py
+++ steam_dmenu.py
@@ -2,24 +2,108 @@
 
 import argparse
 import sys
-import glob
-import re
-from os.path import expanduser
+import vdf
+from os import chmod
+from os import stat as os_stat
+from os.path import expanduser, isdir, isfile
+import stat
 from subprocess import Popen, PIPE
-from typing import List, Tuple
+
+# For larger libraries
+app_name_cache: dict[int, str] = {}
+
+# Configurable
+
+# Blocked AppIds, like tools
+# If not given as argument, the default is used, else the provided one
+blocked_appids: list[int] = []
+
+
+def default_blocked_appids() -> list[int]:
+    # Steamworks
+    blocked_appids = [228980]
+    # Proton
+    blocked_appids += [2348590, 1887720, 858280, 961940, 1054830, 1113280, 1245040, 1493710, 1580130]
+    # Steam Linux Runtime
+    blocked_appids += [1070560]
+    # Steam Linux Runtime - Soldier
+    blocked_appids += [1391110]
+    # Steam Linux Runtime - Sniper
+    blocked_appids += [1628350]
+    # Proton EasyAntiCheat Runtime
+    blocked_appids += [1826330]
+    return blocked_appids
+
 
 def main() -> None:
+    global blocked_appids
+
     args = parse_arguments()
-    dmenu = args.dmenu.replace('$HOME', '~').replace('~', expanduser('~'))
-    returncode, stdout = open_dmenu(dmenu, get_games(args.library))
+
+    library = get_libraryfolders_vdf(args.library)
+
+    if library is None:
+        print('Path does not exist or does not contain the libraryfolder.vdf!')
+    else:
+        vdf_dict = get_vdf_dict(library)
+
+        if args.blocked_appids is None:
+            blocked_appids = default_blocked_appids()
+        else:
+            blocked_appids = args.blocked_appids.split(',')
+            blocked_appids = [int(x) for x in blocked_appids]
+
+        libraries = parse_libraries(vdf_dict)
+
+        if 0 < args.mode:
+            generate_start_scripts(args.output, args.launcher_prefix)
+
+        if 0 == args.mode or 2 == args.mode:
+            start_steam_dmenu(libraries, args.dmenu, args.launcher_prefix)
+
+
+def generate_start_scripts(gen_path: str, dmenu_prefix: str, use_block_list: bool = True) -> None:
+    global app_name_cache, blocked_appids
+
+    if isdir(gen_path):
+        base_path = to_full_userpath(gen_path)
+
+        for appid in app_name_cache:
+            if use_block_list and (appid in blocked_appids):
+                continue
+
+            script_path = '{}/{}{}'.format(base_path, dmenu_prefix, app_name_cache[appid])
+
+            script_file = open(script_path, 'w')
+            command = '#!/bin/bash\nsteam steam://rungameid/{:d}\n'.format(appid)
+            script_file.write(command)
+            script_file.close()
+
+            # Make script executable
+            st = os_stat(script_path)
+            chmod(script_path, st.st_mode | stat.S_IEXEC)
+    else:
+        print('Path does not exist or is not a directory!')
+
+
+def start_steam_dmenu(libraries: dict, args_dmenu: str, dmenu_prefix: str) -> None:
+    apps = parse_apps_for_dmenu(libraries, dmenu_prefix)
+    dmenu = to_full_userpath(args_dmenu)
+
+    returncode, stdout = open_dmenu(dmenu, apps)
     appid = stdout.decode().split(':')[0]
+
     if returncode == 1 or not appid or not appid.isdigit():
         sys.exit()
-    else:
-        launch_game(appid)
+    launch_game(appid)
+
+
+# Utility functions
 
 def parse_arguments() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description='Launch installed Steam games using dmenu')
+    parser = argparse.ArgumentParser(
+        description='Launch installed Steam games using dmenu'
+    )
     parser.add_argument(
         '--dmenu',
         '-d',
@@ -28,56 +112,155 @@ def parse_arguments() -> argparse.Namespace:
         help='Custom dmenu command (default is \'dmenu -i\')'
     )
     parser.add_argument(
-        '--libraries',
+        '--mode',
+        '-m',
+        dest='mode',
+        default=0,
+        type=int,
+        choices=range(0, 3),
+        help='If it should just launch and/or generate scripts (default is 0, just launching)'
+    )
+    parser.add_argument(
+        '--library',
         '-l',
         dest='library',
+        default='~/.local/share/Steam/steamapps/libraryfolders.vdf',
+        help='libraryfolder.vdf location'
+    )
+    parser.add_argument(
+        '--blocked'
+        '-b',
+        dest='blocked_appids',
         nargs='*',
-        default=['~/.local/share/Steam'],
-        help='Steam library location[s] (default is \'~/.local/share/Steam\')'
+        help='Blocked AppId\'s, such as tools like Proton'
+    )
+    parser.add_argument(
+        '--prefix'
+        '-p',
+        dest='launcher_prefix',
+        default='play ',
+        help='Prefix to be used in dmenu (play Team Fortress 2)'
+    )
+    parser.add_argument(
+        '--output',
+        '-o',
+        dest='output',
+        help='Path to generate the script for showing entries in your dmenu'
     )
     parsed_args = parser.parse_args()
 
     return parsed_args
 
-def get_games(libraries: List[str]) -> str:
-    games = []
-    for library in libraries:
-        library = library.replace('$HOME', '~').replace('~', expanduser('~'))
-        apps = glob.glob('{}/steamapps/appmanifest_*.acf'.format(library))
-        for file in apps:
-            with open(file, 'r') as game:
-                lines: List[str] = game.readlines()
-                info = parse_vdf(lines, ['appid', 'name'])
-                games.append('{}: {}'.format(info['appid'], info['name']))
 
-    games.sort(key=lambda x: re.sub('^[0-9]+: ', '', x))
-    return '\n'.join(games)
+def get_libraryfolders_vdf(library_arg: str) -> str or None:
+    if isfile(library_arg):
+        return library_arg
+    elif isdir(library_arg):
+        library = '{}/libraryfolders.vdf'.format(library_arg)
 
-def open_dmenu(dmenu: str, games: str) -> Tuple[int, bytes]:
+        if isfile(library):
+            return library
+
+        library = '{}/steamapps'.format(library_arg)
+
+        if isdir(library):
+            library = '{}/libraryfolders.vdf'.format(library)
+
+            if isfile(library):
+                return library
+    return None
+
+
+def parse_apps_for_dmenu(libraries: dict, dmenu_prefix: str, use_block_list: bool = True) -> str:
+    global blocked_appids
+
+    apps: list[str] = []
+
+    for lib_id in libraries:
+        for appid in libraries[lib_id]['apps']:
+            app_name = libraries[lib_id]['apps'][appid]
+
+            if use_block_list and (appid in blocked_appids):
+                continue
+
+            app = '{}{:d}: {}'.format(dmenu_prefix, appid, app_name)
+            apps.append(app)
+    return '\n'.join(apps)
+
+
+def parse_libraries(lib_dict: vdf.VDFDict) -> dict:
+    if type(lib_dict) is not vdf.VDFDict:
+        lib_dict = vdf_dict_to_dict(lib_dict)
+
+    libraries = dict()
+
+    for lib_key in lib_dict['libraryfolders'].keys():
+        library = dict()
+
+        if isdir(lib_dict['libraryfolders'][lib_key]['path']):
+            library['path'] = lib_dict['libraryfolders'][lib_key]['path']
+        else:
+            continue
+
+        apps = dict()
+
+        for appid in lib_dict['libraryfolders'][lib_key]['apps'].keys():
+            appid = int(appid)
+            app_name = appid_to_app_name(library['path'], appid)
+            apps[appid] = app_name
+        library['apps'] = apps
+        libraries[lib_key] = library
+    return libraries
+
+
+def appid_to_app_name(library_path: str, appid: int) -> str:
+    global app_name_cache
+
+    if app_name_cache.get(appid) is None:
+        app_manifest = '{}/steamapps/appmanifest_{:d}.acf'.format(library_path, appid)
+        app_manifest = vdf_dict_to_dict(get_vdf_dict(app_manifest))
+
+        app_name = app_manifest['AppState']['name']
+        app_name = app_name.encode("ascii", errors="ignore").decode()
+        app_name_cache[appid] = app_name
+        return app_name
+    else:
+        return app_name_cache[appid]
+
+
+def get_vdf_dict(file_path: str) -> vdf.VDFDict:
+    file_path = to_full_userpath(file_path)
+    d = vdf.load(open(file_path, 'r'), mapper=vdf.VDFDict)
+    return d
+
+
+def vdf_dict_to_dict(vdf_dict: vdf.VDFDict) -> dict:
+    vdf_text = vdf.dumps(vdf_dict, pretty=True)
+    return vdf.loads(vdf_text)
+
+
+def dict_to_vdf_dict(vdf_dict: dict) -> vdf.VDFDict:
+    vdf_text = vdf.dumps(vdf_dict, pretty=True)
+    return vdf.loads(vdf_text, mapper=vdf.VDFDict)
+
+
+def to_full_userpath(prefixed_path: str) -> str:
+    return prefixed_path.replace('$HOME', '~').replace('~', expanduser('~'))
+
+
+def launch_game(appid: int) -> None:
+    Popen(['xdg-open', 'steam://run/{}'.format(appid)])
+
+
+def open_dmenu(dmenu: str, apps: str) -> tuple[int, bytes]:
     dmenu = Popen(
         dmenu.split(),
         stdin=PIPE,
         stdout=PIPE
     )
-    (stdout, _) = dmenu.communicate(input=games.encode('UTF-8'))
+    (stdout, _) = dmenu.communicate(input=apps.encode('UTF-8'))
     return dmenu.returncode, stdout
 
-def launch_game(appid: str):
-    Popen(['xdg-open', 'steam://run/{}'.format(appid)])
-
-def parse_vdf(vdf: List[str], keys: List[str]) -> dict:
-    values = {}
-    for line in vdf:
-        matches = re.findall('\t*"(.+?)"', line)
-        if len(matches) == 2 and index_of(keys, matches[0]) >= 0:
-            values[matches[0]] = matches[1]
-    return values
-
-def index_of(list, value) -> int:
-    try:
-        return list.index(value)
-    except ValueError:
-        return -1
 
 if __name__ == "__main__":
     main()
